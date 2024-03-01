a4f2373e5b7bed3bd176beb0f8c44b0e0927dcfd
Reiki <quanqiuyouxiang@126.com>
2024-03-01 14:05:33 +0800

    home-面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanel from '@/views/Home/components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,4 +15,11 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
+<!-- 测试面板组件  有插槽成对出现-->
+  <homePanel title="新鲜好物" subTitle="新鲜好物">
+<div>我是新鲜好物的插槽</div>
+  </homePanel>
+  <homePanel title="人气推荐" subTitle="人气推荐">
+<div>我是人气推荐的插槽</div>
+  </homePanel>
 </template>
