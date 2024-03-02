ef0ca6e4592fd62ed99bb646a678f93d31147475
Reiki <quanqiuyouxiang@126.com>
2024-03-02 12:26:10 +0800

    一级分类-路由配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,7 +19,7 @@ const router = createRouter({
 
                 },
                 {
-                    path: 'category',
+                    path: 'category/:id',
                     component: Category
                 }
             ]
