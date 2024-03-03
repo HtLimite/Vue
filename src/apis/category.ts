135ea44710fa23888518e6610da341ca1843d0fd
Reiki <quanqiuyouxiang@126.com>
2024-03-03 13:12:13 +0800

    二级分类-列表无限加载

--- src/apis/category.ts
+++ src/apis/category.ts
@@ -1,6 +1,7 @@
 //二级分类接口
 import { httpInstance } from '@/utils/http'
 
+// 二级分类列表数据
 function getCategoryAPI(id) {
   return httpInstance({
     url: '/category',
@@ -10,5 +11,34 @@ function getCategoryAPI(id) {
     }
   })
 }
+//二级分类面包屑数据
+function getCategoryFilterAPI(id){
+  return httpInstance({
+    url: '/category/sub/filter',
+    params:{
+      id
+    }
+  })
+}
 
-export { getCategoryAPI }
+//二级分类商品数据
+/**
+ * @description: 获取导航数据
+ * @data {
+ categoryId: 1005000 ,
+ page: 1,
+ pageSize: 20,
+ sortField: 'publishTime' | 'orderNum' | 'evaluateNum'
+ }
+ * @return {*}
+ */
+function getSubCategoryAPI(data){
+  return httpInstance({
+    url:'/category/goods/temporary',
+    method:'POST',
+    data
+  })
+}
+
+
+export { getCategoryAPI , getCategoryFilterAPI, getSubCategoryAPI}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,42 +1,49 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
+
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: Layout,
+      // 二级路由
+      children: [
         {
-            path: '/',
-            component: Layout,
-            // 二级路由
-            children: [
-                {
-                  path: '',
-                  component: Home
+          path: '',
+          component: Home
 
-                },
-                {
-                    path: 'category/:id',
-                    component: Category
-                }
-            ]
         },
         {
-            path: '/login',
-            component: Login
+          path: 'category/:id',
+          component: Category
+        },
+        //二级分类
+        {
+          path: 'category/sub/:id',
+          component: SubCategory
         }
-        // {
-        //   path: '/about',
-        //   name: 'about',
-        //   // route level code-splitting
-        //   // this generates a separate chunk (About.[hash].js) for this route
-        //   // which is lazy-loaded when the route is visited.
-        //   component: () => import('../views/AboutView.vue')
-        // }
-    ]
+      ]
+    },
+    {
+      path: '/login',
+      component: Login
+    }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
+  ]
 })
 
 export default router
